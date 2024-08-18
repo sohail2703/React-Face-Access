326cac90bde817e96bef6c617bbe5835f5bc23b4
sohai <sohailsadiqshaik@gmail.com>
2024-08-18 13:04:56 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      Created via 💙{" "}
+      <a
+        href="https://github.com/BilalGumus"
+        target="_blank"
+        alt="Sohail's GitHub Account"
+        className="hover:underline"
+      >
+        Shaik Sohail
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
